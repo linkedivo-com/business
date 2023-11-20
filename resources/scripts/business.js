13d07eb7b11ac6283d06815da966af5ed724c0ad
ivo-7 <m.bahiraie92@gmail.com>
2023-11-21 01:19:48 +0400

    Update Projects

--- resources/scripts/business.js
+++ resources/scripts/business.js
@@ -1,46 +1,18 @@
-"business" = {
-    "info": {
-        "defaultLang": "true",
-        "direction": "ltr",
-        "language": "en",
-        "referral": "ivo-7",
-        "ivoCode": "ivo-1000",
-        "ivoQRCode": "https://linkedivo.com/linkedivo/images/ivo-1000.svg",
-        "id": "1000",
-        "userName": "linkedivo",
-        "name": "linkedivo",
-        "slogan": "where connection happen",
-        "metaDescription": "Linkedivo is a revolutionary business networking platform designed to connect businesses and individuals, offering a simple and efficient way to find their needs and connect with like-minded professionals. Our platform serves as a comprehensive hub, facilitating seamless collaborations and unlocking new opportunities for growth and success.",
-        "country": "canada",
-        "province": "ontario",
-        "city": "Ottawa",
-        "url": "https://linkedivo.com",
-        "logo": "https://linkedivo.com/linkedivo/images/logo.svg"
-    },
-    "categories": {
-        "IT": ["programming", "web design"],
-        "Marketing": ["Digital Marketing", "Advertisement", "printing"]
-    },
-    "services": {
-        "service1": {
-            "title": "service 1 title",
-            "description": "service 1 description",
-            "image": "https://linkedivo.com/linkedivo/images/service-1.png"
-        },
-        "service2": {
-            "title": "service 2 title",
-            "description": "service 2 description",
-            "image": "https://linkedivo.com/linkedivo/images/service-2.png"
+
+
+async function fetchBusinessInformation() {
+    try {
+        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=45a181ae25dc409faac185842230811&q=London&aqi=no');
+        if (!response.ok) {
+            console.log('we have error')
         }
-    },
-    "socialMedia": {
-        "linkedivo": "https://linkedivo.com/linkedivo",
-        "instagram": "https://instagram.com/linkedivo",
-        "facebook": "https://facebook.com/linkedivo",
-        "linkedin": "https://linkedin.com/linkedivo",
-        "whatsapp": "https://wa.me/+96891711527",
-        "telegram": "https://t.me/+96891711527",
-        "youtube": "https://youtube.com/linkedivo",
-        "twitter": "https://x.com/linkedivo"
+        const data = await response.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        // You can handle errors or return a default value here if needed
+        return error;
     }
 }
+
+fetchBusinessInformation();
